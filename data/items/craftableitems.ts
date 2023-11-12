87c565391f73af81fe03178c769a5e68eb7220bd
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2023-11-12 03:15:38 -0300

    fix vercel

--- data/items/craftableitems.ts
+++ data/items/craftableitems.ts
@@ -2,4 +2,4 @@ import { items } from "./items";
 
 // create an array that contains all the items that can be crafted
 
-export const craftableItems = items.filter(item => item.craftable === true);
+export const craftableItems = items.filter(item => 'craftable' in item && item.craftable === true);
